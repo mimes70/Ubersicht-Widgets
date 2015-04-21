706bc579f7e8d74ec2a5f241f1bd268536e35e12
Miguel Mesquita <Miguel.A.Mesquita@gmail.com>
2015-04-21 08:55:12 +0100

    removida validação de certificados LH

--- links.widget/lighthouse.sh
+++ links.widget/lighthouse.sh
@@ -4,8 +4,8 @@ source "links.widget/enviroment.sh"
 #Request
 #echo -n "Ligthhouse, "
 ##date "+%d %h %H:%M:%S"
-NEW=$(curl -H "X-LighthouseToken: $LH_TOKEN" https://cob.lighthouseapp.com/projects/70149/bins/346069.xml 2>/dev/null | grep "tickets-count"  | sed -e 's/^.*>\(.*\)<.*$/\1 /g')
-NEW2=$(curl -H "X-LighthouseToken: $LH_TOKEN" https://cob.lighthouseapp.com/projects/70911/bins/347835.xml 2>/dev/null | grep "tickets-count"  | sed -e 's/^.*>\(.*\)<.*$/\1 /g')
+NEW=$(curl -k -H "X-LighthouseToken: $LH_TOKEN" https://cob.lighthouseapp.com/projects/70149/bins/346069.xml 2>/dev/null | grep "tickets-count"  | sed -e 's/^.*>\(.*\)<.*$/\1 /g')
+NEW2=$(curl -k -H "X-LighthouseToken: $LH_TOKEN" https://cob.lighthouseapp.com/projects/70911/bins/347835.xml 2>/dev/null | grep "tickets-count"  | sed -e 's/^.*>\(.*\)<.*$/\1 /g')
 TOTAL=`expr $NEW + $NEW2`
 if [[ $TOTAL -eq 1 ]]; then
 	$(say -v Joana "Atenção, 1 tícate novo")
